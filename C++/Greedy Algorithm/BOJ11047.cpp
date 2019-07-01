eebb5492f6a61679ba54749626127bcf38b01aa8
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-07-01 14:10:46 +0900

    190701
    Greedy Algorithm

---
+++ C++/Greedy Algorithm/BOJ11047.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+//BOJ 11047
+
+int main()
+{
+	int N, K, count = 0; // N = 준규가 가지고 있는 동전 종류. K = 현금, count = 동전 개수 최소값
+	scanf("%d %d", &N, &K);
+	vector<int> v(N);
+
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &v[i]);
+	}
+	int i = N-1;
+	while(K>0) {
+		if (K < v[i]) { //K 가 현재 동전보다 클경우 그 동전을 택할 수 없음
+			i--;
+			continue;
+		}
+		K -= v[i];
+		count++;
+	}
+	printf("%d", count);
+	v.clear();
+}
---
+++ C++/Greedy Algorithm/BOJ1449.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+//BOJ1449
+bool water[1001]; //막혀있으면 true
+int main()
+{
+	int N, L, count=0;
+	scanf("%d %d", &N, &L); //물이 새는 곳 N, 테이프 길이 L
+	vector<int> arr(N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d", &arr[i]);
+	}
+	sort(arr.begin(), arr.end()); //정렬
+	for (int i = 0; i < N; i++) {
+		if (water[arr[i]]) { //만약 테이프로 막혀있으면 붙일 필요가 없으므로 그냥 지나간다
+			continue;
+		}
+		for (int j = 0; j < L; j++) { //테이프의 길이만큼 붙인다
+			water[arr[i] + j] = true;
+		}
+		count++;
+	}
+
+	printf("%d", count);
+
+	arr.clear();
+}
