30f9e253141fcecc80d429cbecb192e4363b21b6
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-07-02 16:12:27 +0900

    190702
    greedy algorithm 공부

---
+++ C++/Greedy Algorithm/BOJ11000.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+//BOJ 1931
+
+
+
+int main()
+{
+	int N, x,y;
+	scanf("%d", &N);
+
+	vector<pair<int, int>> v(N);
+	for (int i = 0; i < N; i++) {
+		scanf("%d %d", &x, &y);
+		v[i] = make_pair(x, y);
+	}
+
+	sort(v.begin(), v.end()); // 시작을 오름 차순으로 nlogn
+
+	priority_queue<int, vector<int>, greater<int>> pq;  //greater => 작은 수부터
+	int count = 0;
+	int end = 0;
+
+	pq.push(v[0].second);
+	for (int i = 1; i < N; i++) {
+		if (pq.top() > v[i].first) {
+			pq.push(v[i].second);
+		}
+		else
+		{
+			pq.pop();
+			pq.push(v[i].second);
+		}
+	}
+
+	printf("%d", pq.size());
+	return 0;
+}
---
+++ C++/Greedy Algorithm/BOJ1931.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+//BOJ 1931
+
+
+bool cmp(const pair<int, int> &a, const pair<int, int> &b) {
+	if (a.second == b.second) { //종료시간이 같을 때
+		return a.first < b.first;
+	}	//종료시간이 다를 때
+	return a.second < b.second;
+}
+
+int main()
+{
+	int N;
+	int x, y;
+	scanf("%d", &N);
+	vector<pair<int, int>> arr(N);
+	for (int i = 0; i < N; i++) {
+		
+		scanf("%d %d", &x, &y);
+		pair<int, int> p = make_pair(x, y);
+		arr[i] = p;
+	}
+
+	sort(arr.begin(), arr.end(), cmp);
+
+
+	int count = 0;
+	int maxtime = 0;
+	for (int i = 0; i < N; i++) {
+		if (maxtime <= arr[i].first) {
+			maxtime = arr[i].second;
+			count++;
+		}
+	}
+	printf("%d\n", count);
+	
+	return 0;
+}
