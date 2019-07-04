bd84d5816fbd3b86db5b5827a65f2350064e8cbe
kkw01234 <44748071+kkw01234@users.noreply.github.com>
2019-07-04 17:47:35 +0900

    190704
    dynamic programming

---
+++ C++/Dynamic Programming/BOJ 2193.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+//BOJ 2193d
+
+long long arr[100][2]; //0 => 첫자리가 0일때 , 1=> 첫자리가 1일 때
+//int 형으로는 값이 초과됨 => longlong 이용
+
+int main() //
+{
+	int n;
+	scanf("%d", &n);
+	
+	arr[0][0] = 0, arr[0][1] = 1;
+	arr[1][0] = 1, arr[1][1] = 1;
+	for (int i = 2; i <= n; i++) {
+		arr[i][0] = arr[i - 1][0] + arr[i - 1][1];
+		arr[i][1] = arr[i - 1][0]; //첫자리가 1일 때는 뒤에 무조건 0이 되어야함 (2번 규칙에 의해)
+	}
+	printf("%lld", arr[n][1]);
+
+
+	return 0;
+}
---
+++ C++/Dynamic Programming/BOJ1463.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+//BOJ 1463
+
+int arr[1000001];
+int main()
+{
+	int n, count = 0, num = 2;
+	scanf("%d", &n);
+	arr[1] = 0;
+	while (n!=1) { //1일경우는 바로 종료시켜준다.
+		if (num % 3 == 0 && num % 2 == 0) {
+			arr[num] = min(arr[num / 3], min(arr[num / 2], arr[num - 1])) + 1;
+		}
+		//종료조건
+		else if (num % 2 == 0) { //짝수일때
+			arr[num] = min(arr[num/2],arr[num-1]) +1;
+		}
+		else if (num % 3 == 0) { //3의 배수일때
+			arr[num] = min(arr[num / 3],arr[num-1]) + 1;
+		}
+		else
+			arr[num] = arr[num - 1] + 1;
+		
+		if (num == n) {
+			break;
+		}	
+		num++;
+	}
+
+	printf("%d", arr[num]);
+	return 0;
+}
